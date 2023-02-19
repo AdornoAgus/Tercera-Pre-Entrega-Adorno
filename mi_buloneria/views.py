e40f9a1e80837011963d2cc35cfe958d9c1e6dcb
AdornoAgus <agustinadorno1999@gmail.com>
2023-02-19 15:10:33 -0300

    v1.4 todo funciona

--- mi_buloneria/views.py
+++ mi_buloneria/views.py
@@ -10,7 +10,7 @@ from mi_buloneria.forms import *
 #    return render(request, 'template/inicio.html')
 
 def inicio(self):
-    miHtml=open("C:/Users/Admin/Desktop/bulonera_agustin/buloneria/GitHubBulo/mi_buloneria/template/inicio.html")
+    miHtml=open("mi_buloneria/template/inicio.html")
     plantilla= Template(miHtml.read())
     miHtml.close()
     miContexto=Context()
@@ -26,30 +26,46 @@ def cliente(request):
             informacion = mi_formulario.cleaned_data
             cliente_ = Cliente(nombre=informacion['nombre'], email=informacion['mail'])
             cliente_.save()
-            return render(request, "template/inicio.html")
+            return render(request, "inicio.html")
         
     else:
         mi_formulario =Form_Cliente()
         
-    return render(request, "template/cliente.html", {"mi_formulario":mi_formulario}) 
+    return render(request, "cliente.html", {"mi_formulario":mi_formulario}) 
         
     
 
-def proveedor(self):
-    miHtml=open("C:/Users/Admin/Desktop/bulonera_agustin/buloneria/GitHubBulo/mi_buloneria/template/proveedores.html")
-    plantilla= Template(miHtml.read())
-    miHtml.close()
-    miContexto=Context()
-    documento = plantilla.render(miContexto)
-    return HttpResponse(documento)
+def proveedor(request):
+    if request.method == 'POST':
+        mi_formulario = Form_Proveedor(request.POST)
+        print(mi_formulario)
+        
+        if mi_formulario.is_valid:
+            informacion = mi_formulario.cleaned_data
+            proveedor_ = Proveedor(nombre=informacion['nombre'], rubro=informacion['rubro'])
+            proveedor_.save()
+            return render(request, "inicio.html")
+        
+    else:
+        mi_formulario =Form_Proveedor()
+        
+    return render(request, "proveedores.html", {"mi_formulario":mi_formulario})
 
-def deposito_(self):
-    miHtml=open("C:/Users/Admin/Desktop/bulonera_agustin/buloneria/GitHubBulo/mi_buloneria/template/deposito.html")
-    plantilla= Template(miHtml.read())
-    miHtml.close()
-    miContexto=Context()
-    documento = plantilla.render(miContexto)
-    return HttpResponse(documento)
-
+def deposito_(request):
+    if request.method == 'POST':
+        mi_formulario = Form_Deposito(request.POST)
+        print(mi_formulario)
+        
+        if mi_formulario.is_valid:
+            informacion = mi_formulario.cleaned_data
+            depositos_ = Deposito(item=informacion['item'], cantidad=informacion['cantidad'], marca=informacion['marca'])
+            depositos_.save()
+            return render(request, "inicio.html")   
+    else:
+       mi_formulario=Form_Deposito()
+       
+    return render(request, "deposito.html", {"mi_formulario":mi_formulario})
+        
+    
 
 
